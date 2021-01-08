b632cd2b52528c86c3bdfff2dbd1a0ecbdd9cc42
Eddie Saunders <saunders.eddie@outlook.com>
2021-01-08 07:35:50 -0500

    Removedf archive folder

--- archive/src/pages/Login.js
+++
@@ -1,1 +0,0 @@
-// Login page, this is called depending on what the user selects on the home page
--- archive/src/pages/NewAccount.js
+++
@@ -1,2 +0,0 @@
-// If the user selects new account they land here
-// Nothing too much required, user details, carrier name, email
